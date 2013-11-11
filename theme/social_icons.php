fccb3d7dcdb32575cf991b9f297b39527719d597
Arpita Raj Gupta <arpita.iiit@gmail.com>
2013-11-11 21:43:15 +0530

    Social icons added to the page gnome.org
    https://bugzilla.gnome.org/show_bug.cgi?id=698459
    
    Signed-off-by: Andreas Nilsson <anilsson@redhat.com>

---
+++ theme/social_icons.php
@@ -0,0 +1,20 @@
+<div class="social_network_icons">
+        <ul>
+                <a href="http://identi.ca/gnome">
+                    <img src="<?php bloginfo('template_url')?>/images/social_networks/identica.png" alt=" " />
+                    
+                </a>
+                <a href="http://twitter.com/gnome">
+                    <img src="<?php bloginfo('template_url')?>/images/social_networks/twitter.png" alt=" " />
+                   
+                </a>
+                <a href="http://www.facebook.com/GNOMEDesktop">
+                    <img src="<?php bloginfo('template_url')?>/images/social_networks/facebook.png" alt=" " />
+                    
+                </a>
+                <a href="https://plus.google.com/108054458791366257368/posts">
+                    <img src="<?php bloginfo('template_url')?>/images/social_networks/gplus.png" alt="GNOME on Google+" rel="publisher" />
+                    
+                </a>
+        </ul>
+</div>
