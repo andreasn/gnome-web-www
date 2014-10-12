883f1d0ede7e91b70e86809a7c604649776b27a6
Fabiana Simões <fabianapsimoes@gmail.com>
2014-10-12 12:35:33 +0200

    Membership: updates source for members list

--- theme/page-membership.php
+++ theme/page-membership.php
@@ -3,14 +3,14 @@
 
 if (false === ($members = get_transient('foundation_members_list'))) {
 
-    $members_url = "https://www.gnome.org/membership/members-list.php?format=json";
+    $members_url = "https://foundation.gnome.org/membership/membershiplist";
 
     $members = json_decode(file_get_contents($members_url));
-    
+
     // keeps a 12-hour cache until another HTTP request
     // to get the members list
     set_transient('foundation_members_list', $members, 60*60*12);
-    
+
 }
 
 require_once("header.php"); ?>
@@ -20,38 +20,36 @@ require_once("header.php"); ?>
         <div class="container_12">
 	  	<div class="page_title" style="margin-bottom: 2px;">
 	  	  <h1><?php the_title(); ?></h1>
-		</div>        
+		</div>
 		<div class="clearfix"></div>
             <?php wp_nav_menu(array('menu'=>'foundationnav','container_class'=>'foundation_nav')); ?>
             <div class="content without_sidebar">
             <?php while ( have_posts() ) : the_post(); ?>
                 <?php the_content(); ?>
-                
+
                 <?php
-                
+
                 if (isset($members)) {
                   echo '<ul class="foundation_members_list">'."\n";
-                  $antispam = array(".", "@");
                   foreach ($members as $member) {
-                      $email = str_replace($antispam, " ", $member->email);
-                      echo "    <li title=\"&lt;" . $email . "&gt; / " . "Last Renewed on " . $member->last_renewed_on . "\">" . $member->firstname . " " . $member->lastname . "</li>\n";
+                      echo "<li>" . $member->common_name . "</li>\n";
                   }
                   echo '</ul>'."\n";
                 }
-                
+
                 ?>
-                
+
             <?php endwhile; // End the loop. Whew. ?>
                 <br />
                 <div class="clear"></div>
             </div>
-            
+
             <?php require_once("footer_art.php"); ?>
         </div>
     </div>
-    
+
     <div class="clearfix"></div>
-    
+
     <?php require_once("footer.php"); ?>
 </body>
 </html>
